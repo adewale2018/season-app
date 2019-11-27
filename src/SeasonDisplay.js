b13d4d65b9ddc94bb34326e6e0028984ef5b0961
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-11-27 17:05:17 +0100

    Add display component

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SeasonDisplay = () => {
+  return (
+    <div>
+      <h2>Hi, sason display component!!</h2>
+    </div>
+  );
+};
+
+export default SeasonDisplay;
